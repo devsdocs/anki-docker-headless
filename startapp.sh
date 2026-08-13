0f75aabc56492cb4c5c632442e30a39e387cadb6
devsdocs <id45.go@gmail.com>
2026-08-13 20:23:13 +0900

    Fix proxy issues for QtWebEngine and user mime types

--- startapp.sh
+++ startapp.sh
@@ -6,6 +6,13 @@ export ANKI_DATA_DIR=/config/.local/share/Anki2
 export ANKICONNECT_ID=2055492159
 export BROWSER=/bin/true
 
+# Prevent QtWebEngine from routing localhost requests through any configured proxies
+export no_proxy="localhost,127.0.0.1,::1"
+export NO_PROXY="localhost,127.0.0.1,::1"
+
+# Guarantee the .mjs MIME type is available to the Python server in the user's home directory
+echo "application/javascript mjs" >> /config/.mime.types
+
 
 # Start Anki
 exec anki
